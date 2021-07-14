136195e87a58975cd76185cc18fd9d867917de6f
LonziSlovakia <72870876+LonziSlovakia@users.noreply.github.com>
2021-07-14 11:03:52 +0200

    stránka fičí ale funkcie nejdu hlavne meniť farby

--- src/components/TaskComponent.js
+++ src/components/TaskComponent.js
@@ -5,85 +5,89 @@ import { FiXSquare } from "react-icons/fi";
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
-function Task(task) {
-    // const this.props.color = 'purple';
-    const softDeletedClasses = task.softDeleted ? `bg-${this.props.color}-100` : `bg-${this.props.color}-300`
-    const softDeletedIcon = task.softDeleted ? `bg-red-300 hover:bg-red-400` : `bg-${this.props.color}-300 hover:bg-${this.props.color}-400`
-    const completedClass = task.completed ? 'line-through' : ''
-    const [edit, setEdit] = useState({
-        id: null,
-        value: ''
-    })
-    const taskNameRef = useRef(null)
+class Task extends React.Component {
+    constructor(task) {
+        super();
+        this.taskNameRef = React.createRef(null)
+        this.state = {edit:''}
+    }
 
-    taskNameRef?.current?.focus()
-
-    const handleSoftDeleteTask = (id) => {
+    handleSoftDeleteTask = (id) => {
         this.props.softDeleteTodo(id);
     }
 
-    const handleDeleteTask = (task) => {
+    handleDeleteTask = (task) => {
         this.props.removeTodo(task.id);
     }
 
-    const handleCompleteTask = () => {
-        this.props.completeTodo(task.id)
+    handleCompleteTask = () => {
+        this.props.completeTodo(this.props.task.id)
     }
 
-    const handleEditTask = () => {
-        setEdit({ id: task.id, value: task.task })
+    handleEditTask = () => {
+        this.setState({ id: this.props.task.id, value: this.props.task.task })
     }
 
-    const handleTaskChange = (value) => {
-        setEdit({ ...edit, value: value.target.value })
+    handleTaskChange = (value) => {
+        this.setState({ ...this.state.edit, value: value.target.value })
     }
 
-    const handleSaveTask = () => {
-        if (taskNameRef.current !== null) {
-            const name = taskNameRef.current.value
+    handleSaveTask = () => {
+        if (this.taskNameRef.current !== null) {
+            const name = this.taskNameRef.current.value
             if (name === '') {
-                setEdit({
+                this.setState({
                     id: null,
                     value: ''
                 })
                 return
             }
-            this.props.editTask(edit.id, name)
-            taskNameRef.current.value = ''
-            setEdit({
+            this.props.editTask(this.state.edit.id, name)
+            this.taskNameRef.current.value = ''
+            this.setState({
                 id: null,
                 value: ''
             })
         }
     }
 
-    const handleKeyDown = (event) => {
+    handleKeyDown = (event) => {
         if (event.key === 'Enter') {
-            handleSaveTask()
+            this.handleSaveTask()
         }
     }
 
-    if (edit.id) {
+    render() {
+        const task = this.props.task
+        const softDeletedClasses = task.softDeleted ? `bg-${this.props.color}-100` : `bg-${this.props.color}-300`
+        const softDeletedIcon = task.softDeleted ? `bg-red-300 hover:bg-red-400` : `bg-${this.props.color}-300 hover:bg-${this.props.color}-400`
+        const completedClass = task.completed ? 'line-through' : ''
+
+        this.taskNameRef?.current?.focus()
+
+        if (this.state.edit.id) {
+            return (
+                <div className={`flex flex-col items-stretch md:items-center md:flex-row bg-${this.props.color}-300 border-b-2 border-t-2 border-${this.props.color}-700`}>
+                    <label className="flex-1 flex items-center m-1">
+                        <input ref={this.taskNameRef} autoFocus type="text" className={`flex-auto m-2 px-3 py-1 rounded-lg bg-input-dark font-medium text-text-primary placeholder-text-secondary focus:outline-none focus:ring-4 focus:ring-${this.props.color}-700 focus:ring-opacity-50`} value={this.state.edit.value} onChange={this.handleTaskChange} onKeyDown={this.handleKeyDown} />
+                    </label>
+                    <button className={`rounded-lg px-3 py-1 m-1 font-medium focus:outline-none focus:ring-4 focus:ring-${this.props.color}-700 focus:ring-opacity-50 active:bg-input-dark`} onClick={() => this.handleSaveTask}>Uložiť zmeny</button>
+                </div>
+            )
+        }
+
         return (
-            <div className={`flex flex-col items-stretch md:items-center md:flex-row bg-${this.props.color}-300 border-b-2 border-t-2 border-${this.props.color}-700`}>
-                <label className="flex-1 flex items-center m-1">
-                    <input ref={taskNameRef} autoFocus type="text" className={`flex-auto m-2 px-3 py-1 rounded-lg bg-input-dark font-medium text-text-primary placeholder-text-secondary focus:outline-none focus:ring-4 focus:ring-${this.props.color}-700 focus:ring-opacity-50`} value={edit.value} onChange={handleTaskChange} onKeyDown={handleKeyDown} />
-                </label>
-                <button className={`rounded-lg px-3 py-1 m-1 font-medium focus:outline-none focus:ring-4 focus:ring-${this.props.color}-700 focus:ring-opacity-50 active:bg-input-dark`} onClick={() => handleSaveTask}>Uložiť zmeny</button>
-            </div>
+            <li className={`py-1 px-2 border-b-2 border-t-2 border-${this.props.color}-700 text-${this.props.color}-900 ${softDeletedClasses} flex items-center`}>
+                <div className={`mr-auto ml-auto break-all ${completedClass}`}>{task.task}</div>
+                <div><input type="checkbox" className={`form-checkbox ml-6 mr-1 mb-1 p-2 rounded focus:outline-none focus:ring-4 focus:ring-${this.props.color}-700 focus:ring-opacity-50`} onChange={() => this.handleCompleteTask} checked={task.completed} /></div>
+                <div className={`hover:bg-${this.props.color}-400 rounded-lg p-1`} onClick={() => this.handleEditTask}><FiEdit2 /></div>
+                <div className={`${softDeletedIcon} rounded-lg p-1`} onClick={() => this.handleSoftDeleteTask}><RiDeleteBin6Line /></div>
+                <div className={`hover:bg-${this.props.color}-400 rounded-lg p-1`} onClick={() => this.handleDeleteTask(task)}><FiXSquare /></div>
+            </li>
         )
     }
-
-    return (
-        <li className={`py-1 px-2 border-b-2 border-t-2 border-${this.props.color}-700 text-${this.props.color}-900 ${softDeletedClasses} flex items-center`}>
-            <div className={`mr-auto ml-auto break-all ${completedClass}`}>{task.task}</div>
-            <div><input type="checkbox" className={`form-checkbox ml-6 mr-1 mb-1 p-2 rounded focus:outline-none focus:ring-4 focus:ring-${this.props.color}-700 focus:ring-opacity-50`} onChange={() => handleCompleteTask} checked={task.completed} /></div>
-            <div className={`hover:bg-${this.props.color}-400 rounded-lg p-1`} onClick={() => handleEditTask}><FiEdit2 /></div>
-            <div className={`${softDeletedIcon} rounded-lg p-1`} onClick={() => handleSoftDeleteTask}><RiDeleteBin6Line /></div>
-            <div className={`hover:bg-${this.props.color}-400 rounded-lg p-1`} onClick={() => handleDeleteTask(task)}><FiXSquare /></div>
-        </li>
-    )
 }
+
 
 const mapStateToProps = state => ({
     color: state.color,
